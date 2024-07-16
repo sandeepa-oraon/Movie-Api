caa73d0f9dd5326fcb03d192909e8e28831b5676
Sandeepa Oraon <sandeepa2004oraon@gmail.com>
2024-07-16 21:32:45 +0530

    v1

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,18 +3,31 @@ import ReactPlayer from 'react-player'
 
 const Main = () => {
     const youtubeTrailer= 'https://www.youtube.com/watch?v=Z5W1aLsgAMI'
+
+    
+
+
     return(
         <>
 
-            <div className="relative w-full mx-auto overflow-hidden mt-0">
-                <ReactPlayer width="105vw" height="830px" className="w-full object-cover scale-150 movie-video"  muted playing loop volume={0} 
-            
-                url={youtubeTrailer} 
-                ></ReactPlayer>
+            <div className="relative w-full mx-auto overflow-hidden mt-0 bg-black opacity-50">
+                <ReactPlayer width="105vw" height="830px" className=" w-full object-cover scale-150 opacity-50 movie-video"  
+                muted playing  loop volume={0} url={youtubeTrailer} 
+                controls={false} pip
+                // onPause={() => {
+                //     playerRef.current.getInternalPlayer().playVideo();
+                // }}
+                >
+                </ReactPlayer>0
+                <div className="info ">
+                    <p className='info-'>Discover about your favorite movies anytime, anywhere with our intuitive movie app! Explore a comprehensive movie database with our feature-rich movie website! Access detailed information on the latest releases, timeless classics, and indie gems.</p>
+                </div>
+
             </div>
         
         </>
     )
 }
+
 
 export default Main
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,22 @@
 
 
 const Nav = () => {
+
+    const gradientBorderStyle = () => {
+        border: '2px solid';
+        borderImage: 'linear-gradient(to right, #f40629, #f4ce10)';
+        borderImageSlice: 1
+    }
     return (
         <>
             <div className="nav absolute">
                 <p className="logo">S</p>
-                <button className="movies">movies</button>
+                <div className="search-movie">
+                    <div className="input-">
+                        <input type="text" style={{gradientBorderStyle}} className="input" placeholder="Search for movies"/>
+                    </div>
+                    <button className="movies">movies</button>
+                </div>
             </div>
         </>
     )
